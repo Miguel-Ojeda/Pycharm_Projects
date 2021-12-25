cefc8b6de3c0e45e18756106c42ce387809db124
Miguel O. A. Asus! <mojeaco@gobiernodecanarias.org>
2021-12-25 13:18:24 +0000

    creo la clase bola y hago el screensaver con la bola en OOP
    Además, es bestial porque, en la versión 5 puedo hacer la animación
    con el número de bolas que quiera con un código súper sencillo...

--- OOP/PYGAME/pygame_4 (OOP).py
+++ OOP/PYGAME/pygame_4 (OOP).py
@@ -13,6 +13,8 @@
 import sys
 import pygame
 import random
+from ball import Ball
+
 from pygame.locals import *   # importa constantes...
 
 # 2 definimos constantes....
@@ -22,8 +24,8 @@ WINDOW_HEIGHT = 600
 WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)  # tupla
 FRAMES_PER_SECOND = 30  # es el límite máximo al que correrá, limitado por un reloj....
 # cosas de la bola....
-BALL_WIDTH_HEIGHT = 100   # al ser la imagen cuadrada, es realmente el tamaño de la imagen....
-N_PIXELS_PER_FRAME = 3
+# BALL_WIDTH_HEIGHT = 100   # al ser la imagen cuadrada, es realmente el tamaño de la imagen....
+# N_PIXELS_PER_FRAME = 3
 
 
 # 3 iniciamos el entorno de pygame
@@ -34,16 +36,18 @@ window = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()   # creamos un objeto de la clase Clock, nos va a servir para limitar veolicidad juego...
 
 # 4 - Load assets: image(s), sound(s), etc.
-ballImage = pygame.image.load("images/ball.png")
+# ballImage = pygame.image.load("images/ball.png")
+
 
 # 5 - Initialize variables
-ball_rect = ballImage.get_rect()
-MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
-MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
-ball_rect.x = random.randrange(MAX_WIDTH)
-ball_rect.y = random.randrange(MAX_HEIGHT)
-xSpeed = N_PIXELS_PER_FRAME
-ySpeed = N_PIXELS_PER_FRAME
+# ball_rect = ballImage.get_rect()
+# MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
+# MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+# ball_rect.x = random.randrange(MAX_WIDTH)
+# ball_rect.y = random.randrange(MAX_HEIGHT)
+# xSpeed = N_PIXELS_PER_FRAME
+# ySpeed = N_PIXELS_PER_FRAME
+aBall = Ball(window)
 
 
 
@@ -70,13 +74,14 @@ while True:
 
 
     # 8 acciones a realizar en cada "frame"...
-    if (ball_rect.x < 0) or (ball_rect.x >= MAX_WIDTH):
-        xSpeed = -xSpeed  # reverse X direction
-    if (ball_rect.y < 0) or (ball_rect.y >= MAX_HEIGHT):
-        ySpeed = -ySpeed  # reverse Y direction
-
-    ball_rect.x += xSpeed
-    ball_rect.y += ySpeed
+    # if (ball_rect.x < 0) or (ball_rect.x >= MAX_WIDTH):
+    #     xSpeed = -xSpeed  # reverse X direction
+    # if (ball_rect.y < 0) or (ball_rect.y >= MAX_HEIGHT):
+    #     ySpeed = -ySpeed  # reverse Y direction
+    #
+    # ball_rect.x += xSpeed
+    # ball_rect.y += ySpeed
+    aBall.move()
 
     # 9 borramos la ventana, antes de volver a redibujar nada...
     # realmente habría q  dibujar el fondo, antes de volver a dibujar los elementos
@@ -84,7 +89,8 @@ while True:
 
 
     # 10 volvemos a "dibujar en la ventana (memoria)" de nuevo los elementos...
-    window.blit(ballImage, ball_rect)    # damos las coordenadas de la esquina superior izquierda...
+    # window.blit(ballImage, ball_rect)    # damos las coordenadas de la esquina superior izquierda...
+    aBall.draw()
 
     # 11 mostramos la pantalla  (AHORA ES CUANDO SE MUESTRA REALMENTE LO QUE ESTÁ GRABADO EN LA VENTANA
     # Obs. que no le indicamos a pygame la ventana, él ya sabe las que ha creado y las muestra
