7ddc393293aa44c98f12277d4c5684785e0d2a34
Miguel O. A. Asus! <mojeaco@gobiernodecanarias.org>
2021-12-25 13:36:01 +0000

    corrijo las sugerencias de pycharm

--- OOP/PYGAME/pygame_5 (OOP).py
+++ OOP/PYGAME/pygame_5 (OOP).py
@@ -1,10 +1,10 @@
 # Next, we’ll build a location-based animation. This code will allow us to
 # move an image diagonally and then have it appear to bounce off the edges
-# of the window. This was a favorite technique of screensavers on old CRTbased
+# of the window. This was a favorite technique of screensavers on old CRT based
 # monitors, to avoid burning in a static image.
 
-# en esta nueva versión simularemos un movimiento de rebote...
-# es lo mismo q pygame_2 pero con rect...
+# En esta nueva versión simularemos un movimiento de rebote...
+# es lo mismo que pygame_2, pero con rect...
 # ahora añadimos el sonido...
 
 # Haremos lo mismo, pero con OOP
@@ -25,7 +25,7 @@ WINDOW_HEIGHT = 600
 WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)  # tupla
 FRAMES_PER_SECOND = 30  # es el límite máximo al que correrá, limitado por un reloj....
 # cosas de la bola....
-# BALL_WIDTH_HEIGHT = 100   # al ser la imagen cuadrada, es realmente el tamaño de la imagen....
+# BALL_WIDTH_HEIGHT = 100 # al ser la imagen cuadrada, es realmente el tamaño de la imagen...
 # N_PIXELS_PER_FRAME = 3
 N_BALLS = 50  # ¡jugaremos ahora con 3 bolas!!!
 
@@ -33,7 +33,7 @@ N_BALLS = 50  # ¡jugaremos ahora con 3 bolas!!!
 pygame.init()   # inicializamos las estructuras
 # Creamos una ventana (surface) para poder dibujar, recibir entradas... y nos devuelve su valor ...
 window = pygame.display.set_mode(WINDOW_SIZE)
-clock = pygame.time.Clock()   # creamos un objeto de la clase Clock, nos va a servir para limitar veolicidad juego...
+clock = pygame.time.Clock()   # creamos un objeto de la clase Clock, nos va a servir para limitar velocidad juego...
 
 # 4 - Load assets: image(s), sound(s), etc.
 # ballImage = pygame.image.load("images/ball.png")
@@ -50,14 +50,13 @@ clock = pygame.time.Clock()   # creamos un objeto de la clase Clock, nos va a se
 
 # Creamos una lista con todas las bolas del juego...!! es muy sencillo
 ball_list = []
-for i in range (0, N_BALLS):
+for i in range(0, N_BALLS):
     aBall = Ball(window)
     ball_list.append(aBall)
 
 
-
-# 6 - Loop forever: estos programas, están dirigidos por eventos.. básicamente es un bucle infinito
-# dentro del blucle:
+# 6 - Loop forever: estos programas, están dirigidos por eventos... Básicamente es un bucle infinito
+# dentro del bucle:
 # 7: mirar eventos,
 # 8 acciones en cada frame,
 # 9 volver a colocar el fondo,
@@ -70,13 +69,12 @@ while True:
 
     # 7 Comprobamos los eventos que han sucedido, y damos respuesta a los mismos
     for event in pygame.event.get():
-        # el usuario apreta el botón de cerrar la ventana
+        # el usuario aprieta el botón de cerrar la ventana
         if event.type == pygame.QUIT:
             pygame.quit()   # borra y libera las estructuras....
             sys.exit()  # se termina el programa
-        elif event.type == pygame.KEYDOWN and event.key== pygame.K_q:
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
             sys.exit()
-
 
     # 8 acciones a realizar en cada "frame"...
     # if (ball_rect.x < 0) or (ball_rect.x >= MAX_WIDTH):
@@ -90,27 +88,17 @@ while True:
         aBall.move()
 
     # 9 borramos la ventana, antes de volver a redibujar nada...
-    # realmente habría q  dibujar el fondo, antes de volver a dibujar los elementos
+    # realmente habría que dibujar el fondo, antes de volver a dibujar los elementos
     window.fill(BLACK)
 
-
     # 10 volvemos a "dibujar en la ventana (memoria)" de nuevo los elementos...
-    # window.blit(ballImage, ball_rect)    # damos las coordenadas de la esquina superior izquierda...
+    # window.blit(ballImage, ball_rect) # damos las coordenadas de la esquina superior izquierda...
     for aBall in ball_list:
         aBall.draw()
 
-    # 11 mostramos la pantalla  (AHORA ES CUANDO SE MUESTRA REALMENTE LO QUE ESTÁ GRABADO EN LA VENTANA
+    # 11 mostramos la pantalla (AHORA ES CUANDO SE MUESTRA REALMENTE LO QUE ESTÁ GRABADO EN LA VENTANA)
     # Obs. que no le indicamos a pygame la ventana, él ya sabe las que ha creado y las muestra
     pygame.display.update()
 
     # 12 ralentizamos, para que el programa no esté siempre activo... nos damos un respiro...
     clock.tick(FRAMES_PER_SECOND)   # o sea, esperamos un tick de reloj...
-
-
-
-
-
-
-
-
-
