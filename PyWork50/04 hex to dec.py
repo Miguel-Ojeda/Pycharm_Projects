6f7e6eb75251462442ba73cc48a148807cb7f869
Miguel Ojeda desde Mac Mini <mojeaco@gobiernodecanarias.org>
2022-01-07 17:18:33 +0000

    termino el ej. 4 de 50 py workouts... con ord y chr

--- PyWork50/04 hex to dec.py
+++ PyWork50/04 hex to dec.py
@@ -8,6 +8,28 @@ def mi_hex_output():
         decnum += valor
     print(decnum)
 
+def mi_hex_output_2():
+    # Utilizaremos char y ordinal
+    # Observar que los números son los dígitos son los caracteres unicode desde 48 hasta 57
+    # Las letras van desde el 65 ('A') hasta 70 ('F')
+    decnum = 0
+    hexnum = input('Enter a hex number to convert: ')
+    # Pasamos la cadena a mayúsucla para mirar más fácil
+    hexnum = hexnum.upper()
+    for index, char in enumerate(reversed(hexnum)):
+        # faltan comprobaciones.... pero es solo para pensar...
+        if ord('A') <= ord(char) <= ord('F'):
+            valor = ord(char) - ord('A') + 10
+        elif ord('0') <= ord(char) <= ord('9'):
+            valor = ord(char) - ord('0')
+        else:
+            print('No ha escrito un número hexadecimal... los caracteres válidos son: 0 .. 9 A...F')
+            return
+
+        valor *= 16 ** index
+        decnum += valor
+    print(decnum)
+
 def mi_bin_output():
     decnum = 0
     numero_binario = input('Introduce un número binario: ----> ')
@@ -27,4 +49,5 @@ def mi_bin_output():
 while True:
 
     # mi_hex_output()
-    mi_bin_output()
+    # mi_bin_output()
+    mi_hex_output_2()
