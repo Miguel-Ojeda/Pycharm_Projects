dafb7ad701df91f49310a5e3f366158b60af754a
Miguel O. A. Asus! <mojeaco@gobiernodecanarias.org>
2022-01-08 09:21:56 +0000

    avanzo con el juego tic tac toe... me queda sólo la AI del ordenador

--- ATBS/ch 05/tic_tac_toe.py
+++ ATBS/ch 05/tic_tac_toe.py
@@ -31,12 +31,6 @@ CHAR_T_INV = '┴'
 # U 2536 cruz ┼
 CHAR_CRUZ = '┼'
 
-# TURNOS
-TURNO_JUGADOR = 0
-TURNO_ORDENADOR = 1
-
-
-
 class Tic_Tac_Toe_Game:
     def __init__(self):
         # El tablero es simplemente una lista de 9 items, cuyo contenido está inicialmente vacío
@@ -54,11 +48,20 @@ class Tic_Tac_Toe_Game:
         print('La numeración de las casillas es la siguiente...')
         self.print_board()
 
-        # Ahora borramos todo el tablero para la nueva partida...
-        self.board = [CHAR_VACIO for i in range(9)]
-
         # Cómo se llama el jugador...
         self.player_name = input('¿Cómo te llamas? --> ')
+
+        # Otras variables que nos harán falta; las inicializaremos en iniciar_partida....
+        self.empty_cells = self.ultima_jugada = self.computer_char = self.player_char = self.turno_jugador = None
+
+    def iniciar_partida(self):
+        # Borramos el tablero para la nueva partida...
+        self.board = [CHAR_VACIO for i in range(9)]
+
+        # Configuramos como disponibles actualmente todas las celdas
+        self.empty_cells = [i for i in range(9)]
+
+        # Elegimos ahora las piezas y se sortea el turno...
         while True:
             eleccion = input(f'¿Con qué piezas prefieres jugar, {self.player_name}?  {CHAR_O} ... {CHAR_X} ---> ')
             eleccion = eleccion.upper()
@@ -77,73 +80,50 @@ class Tic_Tac_Toe_Game:
         print(f'{self.player_name}, juegas con {self.player_char}. El ordenador jugará con {self.computer_char}.')
         print('Hagamos el sorteo para ver quién empieza primero...')
         time.sleep(2)
-        eleccion = random.choice((TURNO_JUGADOR, TURNO_ORDENADOR))
-        if eleccion == TURNO_JUGADOR:
+        self.turno_jugador = random.choice((True, False))
+
+
+        if self.turno_jugador:
             print(f'Empiezas tú... Adelante {self.player_name}')
-            self.turno = TURNO_JUGADOR
         else:
             print('Empieza el ordenador... Buena suerte')
-            self.turno = TURNO_ORDENADOR
 
-    def print_board(self, elemento_a_imprimir='BOARD'):
-        if elemento_a_imprimir == 'BOARD':
-            size = 3
-            # Imprimimos el borde superior del board...
-            print(CHAR_ESI + size * CHAR_LH + CHAR_T + size * CHAR_LH + CHAR_T + size * CHAR_LH + CHAR_ESD)
-            # Imprimimos los valores de la primera fila del tablero
-            print(CHAR_LV + ' ' + self.board[0] + ' ' +
-                  CHAR_LV + ' ' + self.board[1] + ' ' +
-                  CHAR_LV + ' ' + self.board[2] + ' ' + CHAR_LV)
-            # imprimimos el separador para la segunda fila
-            print(CHAR_LVD + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_LVI)
-            # Imprimimos los valores de la segunda fila del tablero
-            print(CHAR_LV + ' ' + self.board[3] + ' ' +
-                  CHAR_LV + ' ' + self.board[4] + ' ' +
-                  CHAR_LV + ' ' + self.board[5] + ' ' + CHAR_LV)
-            # imprimimos el separador para la tercera fila
-            print(CHAR_LVD + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_LVI)
-            # Imprimimos los valores de la tercera fila del tablero
-            print(CHAR_LV + ' ' + self.board[6] + ' ' +
-                  CHAR_LV + ' ' + self.board[7] + ' ' +
-                  CHAR_LV + ' ' + self.board[8] + ' ' + CHAR_LV)
-            # Imprimimos el borde inferior del tablero...
-            print(CHAR_EII + size * CHAR_LH + CHAR_T_INV + size * CHAR_LH + CHAR_T_INV + size * CHAR_LH + CHAR_EID)
+    def print_board(self):
+        size = 3
+        # Imprimimos el borde superior del board...
+        print(CHAR_ESI + size * CHAR_LH + CHAR_T + size * CHAR_LH + CHAR_T + size * CHAR_LH + CHAR_ESD)
+        # Imprimimos los valores de la primera fila del tablero
+        print(CHAR_LV + ' ' + self.board[0] + ' ' +
+              CHAR_LV + ' ' + self.board[1] + ' ' +
+              CHAR_LV + ' ' + self.board[2] + ' ' + CHAR_LV)
+        # imprimimos el separador para la segunda fila
+        print(CHAR_LVD + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_LVI)
+        # Imprimimos los valores de la segunda fila del tablero
+        print(CHAR_LV + ' ' + self.board[3] + ' ' +
+              CHAR_LV + ' ' + self.board[4] + ' ' +
+              CHAR_LV + ' ' + self.board[5] + ' ' + CHAR_LV)
+        # imprimimos el separador para la tercera fila
+        print(CHAR_LVD + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_CRUZ + size * CHAR_LH + CHAR_LVI)
+        # Imprimimos los valores de la tercera fila del tablero
+        print(CHAR_LV + ' ' + self.board[6] + ' ' +
+              CHAR_LV + ' ' + self.board[7] + ' ' +
+              CHAR_LV + ' ' + self.board[8] + ' ' + CHAR_LV)
+        # Imprimimos el borde inferior del tablero...
+        print(CHAR_EII + size * CHAR_LH + CHAR_T_INV + size * CHAR_LH + CHAR_T_INV + size * CHAR_LH + CHAR_EID)
 
-
-
-            # for casilla, contenido in enumerate(self.board):
-            #     print(f'Casilla {casilla}: {contenido}')
-            return
-
-        # Tenemos que imprimir alguna fila, columna o diagonal....
-        # El diccionario self.estructura[elemento_a_imprimir] tiene la tupla con las casillas a imprimir!!!
-        # Antes de nada, comprobar que nos han dicho algo válido... claro, como row 1, etc...
-        # bastaría poner self.estructura, pq el defecto son las keys....
-        if elemento_a_imprimir not in self.estructura.keys():
-            print(f'No puedo imprimir la estructura "{elemento_a_imprimir}",' 
-                  f'porque no es una estrucura válida del juego Tic Tac Toe')
-            print('Las estructuras válidas son: row 1, ... row 3, column 1, .... column 3, diagonal 1, diagonal 2')
-            return
-
-        print(f'Imrpimiendo estructura "{elemento_a_imprimir}"... ->', end=' ')
-        for casilla in self.estructura[elemento_a_imprimir]:
-            print(self.board[casilla], end=' ')
-        print('\n')
-
-    def play_next_move(self):
-        if self.turno == TURNO_JUGADOR:
-            self.play_jugador()
-            self.turno = TURNO_ORDENADOR
-        else:
-            self.play_ordenador()
-            self.turno = TURNO_JUGADOR
-
-    def get_empty_cells(self):
-        vacios = tuple(i for i in range(9) if self.board[i] == CHAR_VACIO)
-        # también podría haber puesto directamente
-        # vacios = [i for i in range(9) if self.board[i] == CHAR_VACIO]
-        # No sé porqué, pero si pongo el paréntesis solo, no se genera una tupla, sino un generator...
-        return vacios
+    def print_estructuras(self):
+        # Imprimmimos información sobre filas, columnas, etc
+        # Para debugging realmente....
+        print('Información por estructuras de tablero...')
+        for key, values_tupla in self.estructura.items():
+            print(f'{key}:', end=' ')
+            for item in values_tupla:
+                caracter = self.board[item]
+                # Para que se vea mejor, si está vacío ponemos un guión
+                if caracter == CHAR_VACIO:
+                    caracter = '-'
+                print(caracter, end=' ')
+            print()
 
     def get_player_move(self):
         while True:
@@ -154,32 +134,74 @@ class Tic_Tac_Toe_Game:
                     return casilla
             except ValueError:  # Es una cadena pero no es posible la conversión a entero...
                 pass
-            # Tanto si ocurre una excepcción como si llegamos aquí pq el número no es del rango adecuado....
-            print('Recuerda, debes introducir un número de casilla válido'
+            # Tanto si ocurre una excepción como si llegamos aquí pq el número no es del rango adecuado....
+            print('Recuerda, debes introducir un número de casilla válido '
                   f'desde la 0 (la primera) hasta la 8 (la última)')
+
+    def cambiar_turno(self):
+        self.turno_jugador = not self.turno_jugador
 
     def play_jugador(self):
         # obtenemos una tupla con las casillas vacías... o una lista... o lo que sea...
-        casillas_vacias = self.get_empty_cells()
         while True:
             opcion_elegida = self.get_player_move()
             # Lo anterior simplemente obtiene un valor entre 0 y 8, no hace ninguna comprobación más...
-            if opcion_elegida not in casillas_vacias:
+            if opcion_elegida not in self.empty_cells:
                 print('Haz elegido una casilla no válida, ya se encuentra ocupada!!')
-                print(f'Las casillas vacías son: {casillas_vacias}')
+                print(f'Las casillas vacías son: {self.empty_cells}')
                 continue
             else:
-                print(f'Bien, haz elegido la casilla "{opcion_elegida}"')
-                self.board[opcion_elegida] = self.player_char
-                return  # Y salimos tb. lógicamente, al hacer return... ya hemos movido!!
+                break
+        # Actualizamos el juego...
+        print(f'Bien, haz elegido la casilla "{opcion_elegida}"')
+        self.board[opcion_elegida] = self.player_char
+        self.empty_cells.remove(opcion_elegida)
+        self.ultima_jugada = opcion_elegida
 
     def play_ordenador(self):
-        pass
+        '''Queremos que tenga AI
+        Criterios para mover serían...:
+        1. Si puede ganar, que mueva ahí
+        2. Si puede evitar que ganemos, pues que lo evite...
+        3. Elegir al azar con prioridad: centro, centro lados, esquinas'''
+        # De momento, simplemente elegimos una casilla al azar...
+        print('Le toca mover al ordenador...')
+        time.sleep(1.5)
+        # Actualizamos el juego...
+        opcion_elegida = random.choice(self.empty_cells)
+        print(f'El ordenador ha elegido la casilla "{opcion_elegida}"')
+        self.board[opcion_elegida] = self.computer_char
+        self.empty_cells.remove(opcion_elegida)
+        self.ultima_jugada = opcion_elegida
+
+    def is_winner_last_move(self):
+        # Primero cogemos el carácter del último movimiento
+        character = self.board[self.ultima_jugada]
+
+        for casillas in self.estructura.values():
+            # Recorremos las estructuras, viendo si la casilla en cuestión está...
+            # Si la casilla jugada no pertenece a la fila, col o lo que sea, pues sigue.. pq no hace falta comprobar
+            if self.ultima_jugada not in casillas:
+                continue
+            # Obtenemos una lista con los contenidos de la estructura
+            contenido_casillas = [self.board[i] for i in casillas]
+            # Si las 3 casillas tienen el carácter buscado, retornamos True....
+            if contenido_casillas.count(character) == 3:
+                return True
+
+        # SI hemos llegado hasta aquí, es que no es jugada ganadora!!!
+        return False
+
+    def is_board_full(self):
+        # Si está full, y nadie ha ganada, significa que será empate!!
+        if self.empty_cells == []:
+            return True
+        else:
+            return False
+
+    # Obs.... no hace falta definir la función es empate... simplemente ya sabemos si alguien ha ganado...
+    # Y si al hacer un movimiento nadie ha ganado, y el tablero está lleno, pues ya estaría...
 
 if __name__ == '__main__':
     juego = Tic_Tac_Toe_Game()
     juego.print_board()
-
-
-
-
--- ATBS/ch 05/tic_tac_toe_main.py
+++ ATBS/ch 05/tic_tac_toe_main.py
@@ -1,11 +1,36 @@
-from tic_tac_toe import Tic_Tac_Toe_Game
+from tic_tac_toe import *
 
 partida = Tic_Tac_Toe_Game()
-
+nueva_partida = True
 # print(partida.get_empty_cells())
 while True:
 
+    if nueva_partida:
+        partida.iniciar_partida()
+        nueva_partida = False
+
     partida.print_board()
-    partida.play_jugador()
+    # partida.print_estructuras()
+
+    if partida.turno_jugador:
+        partida.play_jugador()
+    else:
+        partida.play_ordenador()
+
+    if partida.is_winner_last_move():
+        if partida.turno_jugador:
+            print(f'Felicidades, has ganado, {partida.player_name}')
+        else:
+            print(f'Ufff. has perdido, {partida.player_name}')
+        partida.print_board()
+        nueva_partida = True
+
+    # Habrá empate si nadie ha ganado, pero el tablero está ya lleno!!
+    elif partida.is_board_full():
+        print('La partida ha terminado, ha habido un empate')
+        partida.print_board()
+        nueva_partida = True
+
+    partida.cambiar_turno()
 
 
