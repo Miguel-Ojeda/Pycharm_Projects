79874e12301b79bd688cd02508870b8b2893f17d
Miguel Ojeda desde Mac Mini <mojeaco@gobiernodecanarias.org>
2022-01-18 17:28:24 +0000

    Termino el programa para extraer los datos del PDF

--- pdf to csv/02.py
+++ pdf to csv/02.py
@@ -1,77 +1,79 @@
-# Import the required Module
-import tabula
-from pathlib import Path
+# Módulos....
+
+import tabula  # Hace la extracción de datos
+from pathlib import Path  # Objetos Path
+import PySimpleGUI as sg  # Salida gráfica
 import time
-import PySimpleGUI as sg
 import sys
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-# Elegimos el directorio desde que queremos buscar los PDF
-# directorio_base = 'C:/Users/Angel Ojeda/Documents/Miguel/ejemplos de HLC'
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+# Deshabilitado!!
+logging.disable(logging.CRITICAL)
 
 # Creamos una ventana para elegir la carpeta donde están los PDFs...
-
-# Pendiente recuperar para poner en Input y en folder el último directorio elegido si existe..
-# Si no ponerle ....
-
+sg.theme('Dark Blue 3')
 layout = [[sg.Text("Elija la carpeta donde está los PDFs")],
-          [sg.Input(key='DIRECTORIO', default_text=str(Path.cwd())), sg.FolderBrowse('Explorar')],
-          [sg.Text(text_color='red', key='OUTPUT', border_width=2)],
-          # Utilizaremos esto para mostrar errores...
+          # FILA 2: el cuadro de entrada para el directorio, y el botón de explorar...
+          [sg.Input(key='DIRECTORIO', default_text=''),
+           sg.FolderBrowse('Explorar', initial_folder='')],
+          # FILA 3: Utilizaremos esto para mostrar errores...
+          [sg.Text(text_color='red', key='OUTPUT_ERROR', border_width=2)],
+          # FILA 4: Los botones
           [sg.Button('Aceptar'), sg.Button('Salir')]]
 
 window = sg.Window('PDF a CSV', layout)
 
+# Elegimos el directorio desde donde queremos buscar los PDF
 while True:
     event, values = window.read()
     if event in (sg.WINDOW_CLOSED, 'Salir'):
         window.close()
         sys.exit()
-    # Si llegamos aquí es que apretó el botón  de Aceptar....
+    # Si llegamos aquí es que apretó el botón  de Aceptar
     # Leemos el valor elegido...
     directorio_base = values['DIRECTORIO']
-    # creamos el objeto path con la elección del usuario
+    # Creamos el objeto path con la elección del usuario
+    # path_base = Path(directorio_base).resolve() si necesitáramos además que fuera absoluto
     path_base = Path(directorio_base)
-    # path_base = Path(directorio_base).resolve()
-    # Si quisiéramos asegurarnos de obtener un path absoluto simplemente añadir el método resolve()
     if path_base.exists():
-        # Ya está hecha la elección....
-        break
+        # Ya está hecha la elección..
+        # El método glob nos dará todos los objetos Path que cumplen la condición que le pasamos....
+        numero_de_pdfs = len(list(path_base.glob('*.[pP][dD][fF]')))
+        if not numero_de_pdfs:
+            window['OUTPUT_ERROR'].update('No existe ningún PDF en el directorio')
+            continue
+        else:
+            # Todo_ está bien ahora, tenemos ya nuestro directorio con nuestros PDFS....
+            break
     else:
-        # El directorio NO EXISTE!!
-        window['OUTPUT'].update('El directorio indicado no existe')
+        # El directorio NO EXISTE
+        window['OUTPUT_ERROR'].update('El directorio indicado no existe')
 
 window.close()
 logging.debug(f'El directorio elegido ha sido {str(path_base)}')
-sys.exit()
-
-# creamos un objeto Path
-# path_base = Path(directorio_base)
-# if not path_base.exists():
-#     input('EL directorio no existe')
-#     sys.exit()
+logging.debug(f'Tenemos {numero_de_pdfs} PDFs. para analizar')
+# sys.exit()
 
 # Creamos el subdirectorio para los ficheros CSV
+# Le añadimos el tiempo actual, para no sobreescribir lo que ya hubiera...
 local_time = time.localtime()
 str_time = time.strftime('%y%m%d-%H%M%S')
 path_csv = path_base / str_time
-path_csv.mkdir(exist_ok=True)
+path_csv.mkdir(exist_ok=True)  # Si existiera, (que es casi imposible pq va por tiempo) pues que no pase nada...
 
-# Creamos una nueva ventana para ver el proceso... (no para interactuar)
-layout = [[sg.Text("Elija la carpeta donde está los PDFs")],
-          [sg.Input(key='DIRECTORIO', default_text=str(Path.cwd())), sg.FolderBrowse('Explorar')],
-          [sg.Text(text_color='red', key='OUTPUT', border_width=2)],
-          # Utilizaremos esto para mostrar errores...
-          [sg.Button('Aceptar'), sg.Button('Salir')]]
-
-
-for pdf_file in path_base.glob('*.[pP][dD][fD]'):
+for _, pdf_file in enumerate(path_base.glob('*.[pP][dD][fF]')):
     # Averiguamos el nombre (sin la extensión) del pdf
     nombre_del_fichero = pdf_file.stem
-    # Creamos el objeto path aproopiado para el correspondiente CSV
+    # Creamos el objeto path apropiado para el correspondiente CSV
     csv_file = path_csv / f'{nombre_del_fichero}.csv'
-    print(f'Transformando el fichero {nombre_del_fichero} a CSV')
+    # print(f'Transformando el fichero {nombre_del_fichero} a CSV')
+    # Mostramos el progreso....
+    sg.one_line_progress_meter(f'Procesando {numero_de_pdfs} PDFs...', _ + 1, numero_de_pdfs,
+                               f'Procesando fichero {_ + 1} de {numero_de_pdfs} --> {nombre_del_fichero}',
+                               key='HLC_METER', orientation='h', no_button=True)
     tabula.convert_into(input_path=pdf_file, output_path=str(csv_file), output_format='csv', pages='all')
 
+window.close()
+sg.popup('Proceso terminado')
 
