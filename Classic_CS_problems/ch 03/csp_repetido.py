5e62132273ac48bd5f2a8d1706da2098bd2626e9
Miguel O. A. Asus! <mojeaco@gobiernodecanarias.org>
2022-03-06 10:45:15 +0000

    Classic CS Problems... añado heurísticas al sudoku... muchísimo más rápido

--- Classic_CS_problems/ch 03/csp_repetido.py
+++ Classic_CS_problems/ch 03/csp_repetido.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Generic, TypeVar, Optional
+from typing import List, Dict, Generic, TypeVar, Optional, Callable
 from abc import ABC, abstractmethod
 
 V = TypeVar('V')  # tipo variable
@@ -73,3 +73,66 @@ class CSP(Generic[V, D]):
                     return resultado
         return None  # no haría falta, se sobreentiende... pero por claridad...
 
+
+    def backtracking_search_heuristic(self, heuristic: Callable[[V, Assignment], float],
+                                      assignment: Assignment = {}) -> Optional[Assignment]:
+        '''
+        Modificamos el algoritmo anterior para utilizar una función de heurística...
+        La función heurística nos ayuda a decidir a qué variable vamos a asignar valor
+        En el modo normal, se asigna a la primera variable
+        Aquí utilizaremos la función de heurística asignará a cada una de las variables no asignadas
+        un peso... la que tenga mayor peso, será la que cojamos...
+        '''
+        # algoritmo recursivo...
+        if len(assignment) == len(self.variables):
+            return assignment
+        # Si llegamos hasta aquí, es que queda alguna por asignar...
+        unassigned_vars: List[V] = [var for var in self.variables if var not in assignment]
+        unassigned_pesos: List[float] = [heuristic(var, assignment) for var in unassigned_vars]
+        peso_maximo = max(unassigned_pesos)
+        indice = unassigned_pesos.index(peso_maximo)
+        first_un: V = unassigned_vars[indice]
+
+        # Ahora a ver que valor asignamos de su dominio..
+        for value in self.domains[first_un]:
+            local_assignment = assignment.copy()
+            local_assignment[first_un] = value
+            if self.consistent(first_un, local_assignment):
+                resultado = self.backtracking_search_heuristic(heuristic, local_assignment)
+                if resultado is not None:
+                    return resultado
+        return None  # no haría falta, se sobreentiende... pero por claridad...
+
+
+    def bts_heuristic_con_restriccion(self, heuristic_restrict: Callable[[V, Assignment], tuple[float, Domain]],
+                                      assignment: Assignment = {}) -> Optional[Assignment]:
+        '''
+        Esta heurística es más potente, además de asignar un peso a la variable
+        devuelve el dominio de valores actualizados que debería tener...
+        Con lo que cada vez más se va restringiendo todo mucho más rápido
+        '''
+        # algoritmo recursivo...
+        if len(assignment) == len(self.variables):
+            return assignment
+        # Si llegamos hasta aquí, es que queda alguna por asignar...
+        unassigned_vars: List[V] = [var for var in self.variables if var not in assignment]
+        unassigned_pesos: List[float] = []
+        for var in unassigned_vars:
+            peso, dominio = heuristic_restrict(var, assignment)
+            unassigned_pesos.append(peso)
+            # Actualizamos el dominio!!!!
+            self.domains[var] = dominio
+
+        peso_maximo = max(unassigned_pesos)
+        indice = unassigned_pesos.index(peso_maximo)
+        first_un: V = unassigned_vars[indice]
+
+        # Ahora a ver que valor asignamos de su dominio..
+        for value in self.domains[first_un]:
+            local_assignment = assignment.copy()
+            local_assignment[first_un] = value
+            if self.consistent(first_un, local_assignment):
+                resultado = self.bts_heuristic_con_restriccion(heuristic_restrict, local_assignment)
+                if resultado is not None:
+                    return resultado
+        return None  # no haría falta, se sobreentiende... pero por claridad...
--- Classic_CS_problems/ch 03/sudoku_solver_ex_3.py
+++ Classic_CS_problems/ch 03/sudoku_solver_ex_3.py
@@ -12,6 +12,7 @@ from csp_repetido import CSP, Constraint
 
 Numero_1_al_9 = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
 class SudokuLocation(NamedTuple):
     row: int
     column: int
@@ -54,30 +55,85 @@ class SudokuConstraint(Constraint[List[SudokuLocation], List[Numero_1_al_9]]):
         return True
 
 
+def heuristica(celda: SudokuLocation, assignment: Dict[SudokuLocation, Numero_1_al_9]) -> float:
+    '''básicamente lo que haremos será asignar a la celda, el número de restricciones
+    que posee, o sea, miraremos en las asignaciones y veremos cuantas valores están
+    asignados en su fila, columna y cuadrado...
+    mientras más restricciones tenga, pues mejor, pq teóricamente debería ser más fácil encontrar
+    el valor correcto!!!!
+    '''
+    celdas_asignadas_fila = {SudokuLocation(row=celda.row, column=c)
+                             for c in range(9)
+                             if SudokuLocation(row=celda.row, column=c) in assignment}
+
+    celdas_asignadas_columna = {SudokuLocation(row=r, column=celda.column)
+                                for r in range(9)
+                                if SudokuLocation(row=r, column=celda.column) in assignment}
+
+    # Hallamos ahora la celda donde empieza el cuadrado
+    start_row = (celda.row // 3) * 3
+    start_column = (celda.column // 3) * 3
+
+    celdas_asignadas_cuadrado = {SudokuLocation(row=start_row + dr, column=start_column + dc)
+                                 for dr in (0, 1, 2)
+                                 for dc in (0, 1, 2)
+                                 if SudokuLocation(row=start_row + dr, column=start_column + dc) in assignment}
+
+    celdas_asignadas = celdas_asignadas_cuadrado | celdas_asignadas_columna | celdas_asignadas_cuadrado
+    return len(celdas_asignadas)
+
+
+def heuristica_con_restriccion(celda: SudokuLocation, assignment: Dict[SudokuLocation, Numero_1_al_9]) \
+        -> tuple[float, List[Numero_1_al_9]]:
+    '''Es lo mismo que antes, pero tb devolvemos los valores que puede tomar
+    '''
+    celdas_asignadas_fila = {SudokuLocation(row=celda.row, column=c)
+                             for c in range(9)
+                             if SudokuLocation(row=celda.row, column=c) in assignment}
+
+    celdas_asignadas_columna = {SudokuLocation(row=r, column=celda.column)
+                                for r in range(9)
+                                if SudokuLocation(row=r, column=celda.column) in assignment}
+
+    # Hallamos ahora la celda donde empieza el cuadrado
+    start_row = (celda.row // 3) * 3
+    start_column = (celda.column // 3) * 3
+
+    celdas_asignadas_cuadrado = {SudokuLocation(row=start_row + dr, column=start_column + dc)
+                                 for dr in (0, 1, 2)
+                                 for dc in (0, 1, 2)
+                                 if SudokuLocation(row=start_row + dr, column=start_column + dc) in assignment}
+
+    celdas_asignadas = celdas_asignadas_cuadrado | celdas_asignadas_columna | celdas_asignadas_cuadrado
+    valores_asignados = set(assignment[celda] for celda in celdas_asignadas)
+    dominio = [i for i in [1, 2, 3, 4, 5, 6, 7, 8, 9] if i not in valores_asignados]
+    return len(celdas_asignadas), dominio
+
+
 if __name__ == "__main__":
     locations = [SudokuLocation(row=row, column=column) for row in range(9) for column in range(9)]
     domains: Dict[SudokuLocation, List[Numero_1_al_9]]
     domains = {location: [1, 2, 3, 4, 5, 6, 7, 8, 9] for location in locations}
 
     '''aquí añadiríamos las propias condiciones del tal'''
-    assginment_inicial : Dict[SudokuLocation, Numero_1_al_9]
     # Ejemplo 1
-    assginment_inicial = {SudokuLocation(row=0, column=0): 5, SudokuLocation(row=0, column=1): 3,
-                          SudokuLocation(row=0, column=4): 7, SudokuLocation(row=1, column=0): 6,
-                          SudokuLocation(row=1, column=3): 1, SudokuLocation(row=1, column=4): 9,
-                          SudokuLocation(row=1, column=5): 5, SudokuLocation(row=2, column=1): 9,
-                          SudokuLocation(row=2, column=2): 8, SudokuLocation(row=2, column=7): 6,
-                          SudokuLocation(row=3, column=0): 8, SudokuLocation(row=3, column=4): 6,
-                          SudokuLocation(row=3, column=8): 3, SudokuLocation(row=4, column=0): 4,
-                          SudokuLocation(row=4, column=3): 8, SudokuLocation(row=4, column=5): 3,
-                          SudokuLocation(row=4, column=8): 1, SudokuLocation(row=5, column=0): 7,
-                          SudokuLocation(row=5, column=4): 2, SudokuLocation(row=5, column=8): 6,
-                          SudokuLocation(row=6, column=1): 6, SudokuLocation(row=6, column=6): 2,
-                          SudokuLocation(row=6, column=7): 8, SudokuLocation(row=7, column=3): 4,
-                          SudokuLocation(row=7, column=4): 1, SudokuLocation(row=7, column=5): 9,
-                          SudokuLocation(row=7, column=8): 5, SudokuLocation(row=8, column=4): 8,
-                          SudokuLocation(row=8, column=7): 7, SudokuLocation(row=8, column=8): 9,
-                          }
+    assginment_inicial_1: Dict[SudokuLocation, Numero_1_al_9]
+    assginment_inicial_1 = {SudokuLocation(row=0, column=0): 5, SudokuLocation(row=0, column=1): 3,
+                            SudokuLocation(row=0, column=4): 7, SudokuLocation(row=1, column=0): 6,
+                            SudokuLocation(row=1, column=3): 1, SudokuLocation(row=1, column=4): 9,
+                            SudokuLocation(row=1, column=5): 5, SudokuLocation(row=2, column=1): 9,
+                            SudokuLocation(row=2, column=2): 8, SudokuLocation(row=2, column=7): 6,
+                            SudokuLocation(row=3, column=0): 8, SudokuLocation(row=3, column=4): 6,
+                            SudokuLocation(row=3, column=8): 3, SudokuLocation(row=4, column=0): 4,
+                            SudokuLocation(row=4, column=3): 8, SudokuLocation(row=4, column=5): 3,
+                            SudokuLocation(row=4, column=8): 1, SudokuLocation(row=5, column=0): 7,
+                            SudokuLocation(row=5, column=4): 2, SudokuLocation(row=5, column=8): 6,
+                            SudokuLocation(row=6, column=1): 6, SudokuLocation(row=6, column=6): 2,
+                            SudokuLocation(row=6, column=7): 8, SudokuLocation(row=7, column=3): 4,
+                            SudokuLocation(row=7, column=4): 1, SudokuLocation(row=7, column=5): 9,
+                            SudokuLocation(row=7, column=8): 5, SudokuLocation(row=8, column=4): 8,
+                            SudokuLocation(row=8, column=7): 7, SudokuLocation(row=8, column=8): 9,
+                            }
     '''
     SOLUCIÓN
      5  3  4  6  7  8  9  1  2 
@@ -92,49 +148,49 @@ if __name__ == "__main__":
      '''
 
     # Ejemplo 2
-    '''
-    assginment_inicial = {SudokuLocation(row=0, column=0): 1,
-                          SudokuLocation(row=0, column=4): 9,
-                          # SudokuLocation(row=0, column=6): 8,
+    assginment_inicial_2: Dict[SudokuLocation, Numero_1_al_9]
+    assginment_inicial_2 = {SudokuLocation(row=0, column=0): 1,
+                            SudokuLocation(row=0, column=4): 9,
+                            # SudokuLocation(row=0, column=6): 8,
 
-                          SudokuLocation(row=1, column=0): 9,
-                          SudokuLocation(row=1, column=3): 8,
-                          # SudokuLocation(row=1, column=6): 1,
-                          # SudokuLocation(row=1, column=5): 5,
+                            SudokuLocation(row=1, column=0): 9,
+                            SudokuLocation(row=1, column=3): 8,
+                            # SudokuLocation(row=1, column=6): 1,
+                            # SudokuLocation(row=1, column=5): 5,
 
-                          SudokuLocation(row=2, column=1): 3,
-                          SudokuLocation(row=2, column=2): 2,
-                          SudokuLocation(row=2, column=7): 8,
+                            SudokuLocation(row=2, column=1): 3,
+                            SudokuLocation(row=2, column=2): 2,
+                            SudokuLocation(row=2, column=7): 8,
 
-                          SudokuLocation(row=3, column=4): 8,
-                          SudokuLocation(row=3, column=5): 4,
-                          SudokuLocation(row=3, column=6): 9,
-                          SudokuLocation(row=3, column=7): 3,
+                            SudokuLocation(row=3, column=4): 8,
+                            SudokuLocation(row=3, column=5): 4,
+                            SudokuLocation(row=3, column=6): 9,
+                            SudokuLocation(row=3, column=7): 3,
 
-                          SudokuLocation(row=4, column=4): 2,
-                          SudokuLocation(row=4, column=8): 8,
-                          # SudokuLocation(row=4, column=5): 3,
-                          # SudokuLocation(row=4, column=8): 1,
+                            SudokuLocation(row=4, column=4): 2,
+                            SudokuLocation(row=4, column=8): 8,
+                            # SudokuLocation(row=4, column=5): 3,
+                            # SudokuLocation(row=4, column=8): 1,
 
-                          SudokuLocation(row=5, column=0): 5,
-                          SudokuLocation(row=5, column=1): 2,
-                          SudokuLocation(row=5, column=7): 1,
+                            SudokuLocation(row=5, column=0): 5,
+                            SudokuLocation(row=5, column=1): 2,
+                            SudokuLocation(row=5, column=7): 1,
 
-                          SudokuLocation(row=6, column=2): 4,
-                          SudokuLocation(row=6, column=3): 9,
-                          SudokuLocation(row=6, column=4): 5,
-                          SudokuLocation(row=6, column=6): 8,
+                            SudokuLocation(row=6, column=2): 4,
+                            SudokuLocation(row=6, column=3): 9,
+                            SudokuLocation(row=6, column=4): 5,
+                            SudokuLocation(row=6, column=6): 8,
 
-                          SudokuLocation(row=7, column=1): 5,
-                          SudokuLocation(row=7, column=6): 1,
-                          SudokuLocation(row=7, column=7): 7,
-                          # SudokuLocation(row=7, column=8): 5,
+                            SudokuLocation(row=7, column=1): 5,
+                            SudokuLocation(row=7, column=6): 1,
+                            SudokuLocation(row=7, column=7): 7,
+                            # SudokuLocation(row=7, column=8): 5,
 
-                          SudokuLocation(row=8, column=5): 7,
-                          # SudokuLocation(row=8, column=7): 7,
-                          # SudokuLocation(row=8, column=8): 9,
-                          }
-    '''
+                            SudokuLocation(row=8, column=5): 7,
+                            # SudokuLocation(row=8, column=7): 7,
+                            # SudokuLocation(row=8, column=8): 9,
+                            }
+
     '''SOLUCIÓN EJEMPLO 2"
      1  8  5  6  9  2  3  4  7 
      9  4  6  8  7  3  5  2  1 
@@ -145,89 +201,39 @@ if __name__ == "__main__":
      2  7  4  9  5  1  8  6  3 
      3  5  9  2  6  8  1  7  4 
      8  6  1  3  4  7  2  9  5 
-     
-     El cProfile es el siguiente...
-             194_569_966 function calls (194500610 primitive calls) in 168.419 seconds
+  '''
 
-   Ordered by: standard name
-
-   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
- 84633110   27.782    0.000   44.455    0.000 <string>:1(<lambda>)
-        1    0.000    0.000  168.419  168.419 <string>:1(<module>)
-   623968    0.888    0.000  166.501    0.000 csp_repetido.py:42(consistent)
-  69357/1    0.982    0.000  168.419  168.419 csp_repetido.py:48(backtracking_search)
-    69356    0.565    0.000    0.565    0.000 csp_repetido.py:53(<listcomp>)
-   623968   14.355    0.000  165.613    0.000 sudoku_solver_ex_3.py:24(satisfied)
-  3451980   61.123    0.000   89.366    0.000 sudoku_solver_ex_3.py:28(<setcomp>)
-  3451980    3.677    0.000    3.677    0.000 sudoku_solver_ex_3.py:31(<setcomp>)
-  1446861   24.708    0.000   35.306    0.000 sudoku_solver_ex_3.py:36(<setcomp>)
-  1446861    1.412    0.000    1.412    0.000 sudoku_solver_ex_3.py:39(<setcomp>)
-   733770   13.699    0.000   19.313    0.000 sudoku_solver_ex_3.py:46(<setcomp>)
-   733770    0.740    0.000    0.740    0.000 sudoku_solver_ex_3.py:50(<setcomp>)
- 84633110   16.673    0.000   16.673    0.000 {built-in method __new__ of type object at 0x00007FFE7A21B920}
-        1    0.000    0.000  168.419  168.419 {built-in method builtins.exec}
- 11403936    1.361    0.000    1.361    0.000 {built-in method builtins.len}
-   623968    0.349    0.000    0.349    0.000 {method 'copy' of 'dict' objects}
-        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
-   623968    0.106    0.000    0.106    0.000 {method 'keys' of 'dict' objects}
-     
-     
- '''
-
-
-    csp: CSP[SudokuLocation, int] = CSP(locations, domains)
+    csp: CSP[SudokuLocation, Numero_1_al_9] = CSP(locations, domains)
     csp.add_constraint(SudokuConstraint(locations))
-    solution: Optional[Dict[SudokuLocation, Numero_1_al_9]] = csp.backtracking_search(assginment_inicial)
-    cProfile.run('solution = csp.backtracking_search(assginment_inicial)')
-
+    # solution: Optional[Dict[SudokuLocation, Numero_1_al_9]] = csp.backtracking_search(assginment_inicial)
+    # cProfile.run('solution = csp.backtracking_search(assginment_inicial)')
 
     # solution: Optional[Dict[SudokuLocation, Numero_1_al_9]] = csp.backtracking_search(assginment_inicial)
+    # solution = csp.backtracking_search_heuristic(heuristic=heuristica,  assignment=assginment_inicial_2)
+    # solution = csp.bts_heuristic_con_restriccion(heuristic_restrict=heuristica_con_restriccion,
+    #                                              assignment=assginment_inicial_2)
+
+
+
+    # cProfile.run('csp.backtracking_search(assginment_inicial_1)')
+    # Ejemplo 1: 11_739_020 function calls (11734812 primitive calls) in 9.864 seconds
+    # cProfile.run('csp.backtracking_search_heuristic(heuristic=heuristica,  assignment=assginment_inicial_1)')
+    # Ejemplo 1: 307677 function calls (307609 primitive calls) in 0.229 seconds
+    # En concreto, en la función heurística casi no tarda nada, 0.008 segundos!!
+    # 1975    0.008    0.000    0.099    0.000 sudoku_solver_ex_3.py:58(heuristica)
+    # cProfile.run('csp.backtracking_search(assginment_inicial_2)')
+    # Ejemplo 2: 194_569_966 function calls (194500610 primitive calls) in 167.131 seconds
+    cProfile.run('csp.backtracking_search_heuristic(heuristic=heuristica,  assignment=assginment_inicial_2)')
+    # Ejemplo 2: 2874801 function calls (2874328 primitive calls) in 2.124 seconds
+    # En concreto, la heurística consume poco, 0'072 segundos
+    # 18484    0.072    0.000    0.897    0.000 sudoku_solver_ex_3.py:58(heuristica)
+    cProfile.run('csp.bts_heuristic_con_restriccion(heuristica_con_restriccion, assginment_inicial_2)')
+    # 2050200 function calls (2049727 primitive calls) in 1.501 seconds
+
+    sys.exit()
     if solution is None:
         print("No solution found!")
     else:
         for row in range(9):
             fila = ''.join(str(solution[SudokuLocation(row=row, column=c)]).center(3) for c in range(9))
             print(fila)
-
-    '''
-    domains[SudokuLocation(row=0, column=0)] = [5]
-    domains[SudokuLocation(row=0, column=1)] = [3]
-    domains[SudokuLocation(row=0, column=4)] = [7]
-    domains[SudokuLocation(row=1, column=0)] = [6]
-    domains[SudokuLocation(row=1, column=3)] = [1]
-    domains[SudokuLocation(row=1, column=4)] = [9]
-    domains[SudokuLocation(row=1, column=5)] = [5]
-    domains[SudokuLocation(row=2, column=1)] = [9]
-    domains[SudokuLocation(row=2, column=2)] = [8]
-    domains[SudokuLocation(row=2, column=7)] = [6]
-    domains[SudokuLocation(row=3, column=0)] = [8]
-    domains[SudokuLocation(row=3, column=4)] = [6]
-    domains[SudokuLocation(row=3, column=8)] = [3]
-    domains[SudokuLocation(row=4, column=0)] = [4]
-    domains[SudokuLocation(row=4, column=3)] = [8]
-    domains[SudokuLocation(row=4, column=5)] = [3]
-    domains[SudokuLocation(row=4, column=8)] = [1]
-    domains[SudokuLocation(row=5, column=0)] = [7]
-    domains[SudokuLocation(row=5, column=4)] = [2]
-    domains[SudokuLocation(row=5, column=8)] = [6]
-    domains[SudokuLocation(row=6, column=1)] = [6]
-    domains[SudokuLocation(row=6, column=6)] = [2]
-    domains[SudokuLocation(row=6, column=7)] = [8]
-    domains[SudokuLocation(row=7, column=3)] = [4]
-    domains[SudokuLocation(row=7, column=4)] = [1]
-    domains[SudokuLocation(row=7, column=5)] = [9]
-    domains[SudokuLocation(row=7, column=8)] = [5]
-    domains[SudokuLocation(row=8, column=4)] = [8]
-    domains[SudokuLocation(row=8, column=7)] = [7]
-    domains[SudokuLocation(row=8, column=8)] = [9]
-    '''
-
-
-
-
-
-
-
-
-
-
